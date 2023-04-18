09ba0064482240ea50d11b761ddcb832be384655
nicocastx <nicocastx@gmail.com>
2023-04-18 20:10:48 -0300

    desafio de deno color y lista

--- 24desafio/server.tsx
+++ 24desafio/server.tsx
@@ -5,12 +5,50 @@ import {
   WebApp,
 } from "https://deno.land/x/denorest@v4.2/mod.ts";
 
+
+
 const app = new WebApp();
 const router = new Router();
 
-router.get("/", (_req: Req, res: Res) => {
-  res.reply = `buenas a todos guapisimos`
+app.headers({
+  "Content-Type":"text/html"
+})
+
+let color = ""
+
+const colores: Array<String> = []
+
+function addColor(color){
+  if(!color){
+    return ''
+  }
+  colores.push(color)
+  let coloresHTML = ''
+  colores.forEach(c => {
+    coloresHTML += `<li><p style="color:${c};">${c}</p></li>`
+  });
+  return coloresHTML
+}
+
+router.get("/", (req: Req, res: Res) => {
+  if(req.url.search != undefined){
+    color = req.url.search.split('=')[1]
+  }
+  const html = `
+  <h1>Hola</h1>
+  <p style="color:#${color};">Ingrese un color para cambiar al html de este texto</p>
+  <form action="/" method="GET">
+    <label for="textc">Ingrese el color de la letra:</label><br>
+    <input type="text" id="textc" name="textc"><br>
+    <input type="submit" value="Submit">
+  </form>
+  <ul>
+  ${addColor(color)}
+  </ul>
+  `
+  res.reply = html
 });
+
 
 app.set(router)
 app.listen(8080);
