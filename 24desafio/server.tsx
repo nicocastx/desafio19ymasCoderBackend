6194525798cb3f900eb67e33e665fd85a59e3fdc
nicocastx <nicocastx@gmail.com>
2023-04-18 00:03:16 -0300

    avanze deno

---
+++ 24desafio/server.tsx
@@ -0,0 +1,16 @@
+import {
+  Req,
+  Res,
+  Router,
+  WebApp,
+} from "https://deno.land/x/denorest@v4.2/mod.ts";
+
+const app = new WebApp();
+const router = new Router();
+
+router.get("/", (_req: Req, res: Res) => {
+  res.reply = `buenas a todos guapisimos`
+});
+
+app.set(router)
+app.listen(8080);
